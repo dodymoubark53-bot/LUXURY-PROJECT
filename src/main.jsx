622ad4f96dc9bc4fa37dab32b7a5e95d09bd06b4
Dina M Hefny <dodymoubark53@gmaill.com>
2026-05-18 11:12:57 +0300

    VERCEL

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ console.log('✅ All errors fixed — website running correctly');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/LUXURY-PROJECT">
+    <BrowserRouter>
       <HelmetProvider>
         <AuthProvider>
           <App />
